bb971d2ad503130a3036532af20c86efa26947f6
Arif <arifronyahmed@gmail.com>
2023-11-10 23:22:39 +0100

    connect database

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'))
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// newsletter route
+
+app.post('/api/v1/subscribe', (req, res) => {
+  const { email } = req.body;
+  console.log(email);
+
+  res.status(200).json({
+    status: 'success',
+  });
 });
-
 
 // ***********************************
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,12 @@
-const port = 3000;
+require('dotenv').config({ path: './config.env' });
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE, {}).then(() => {
+  console.log('Connected to MongoDB Cloud');
+});
+
+const port = process.env.PORT || 3000;
 const app = require('./app');
 
 app.listen(port, () => {
